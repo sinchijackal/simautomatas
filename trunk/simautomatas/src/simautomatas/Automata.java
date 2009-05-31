5a55f6ba6419aef92f2ff14a836227599e356aa1
joarobles89 <joarobles89@3ab53680-4b92-11de-a3de-f74ccfcb021d>
2009-05-31 18:04:53 +0000

    agregadas clases: Automata AFD (extiende Automata)

---
+++ trunk/simautomatas/src/simautomatas/Automata.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package simautomatas;
+
+/**
+ *
+ * @author Joaquin
+ */
+public class Automata {
+
+}
